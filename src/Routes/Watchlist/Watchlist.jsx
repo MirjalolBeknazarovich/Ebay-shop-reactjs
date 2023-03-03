fdd5537a6c6606a0a6106215692cf6b3239152c2
MirjalolTurayev <111414345+MirjalolTurayev@users.noreply.github.com>
2023-03-03 12:35:30 +0500

    Exam Final Front end

--- src/Routes/Watchlist/Watchlist.jsx
+++ src/Routes/Watchlist/Watchlist.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const Watchlist = () => {
-  const watchlistProduct = useSelector( state => state.createReducer )
+  const watchlistProduct = useSelector( state => state )
   console.log(watchlistProduct);
   return (
     <div>Watchlist</div>
